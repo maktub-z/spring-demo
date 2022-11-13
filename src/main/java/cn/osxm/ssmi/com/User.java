8fe1c2a28d7faafa0c76e3cbceae00b7075c7c10
zhangxiaolong <mak_tub@163.com>
2022-11-13 16:18:25 +0800

    依赖注入配置简写

---
+++ src/main/java/cn/osxm/ssmi/com/User.java
@@ -0,0 +1,19 @@
+package cn.osxm.ssmi.com;
+
+public class User {
+    private String name;
+    private User user;
+    private Integer age;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
