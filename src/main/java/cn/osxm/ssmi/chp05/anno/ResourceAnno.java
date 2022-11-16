965489be7fcc89a9b2c18b99bbd6d9c90b8f70d7
zhangxiaolong <mak_tub@163.com>
2022-11-17 06:47:46 +0800

    注释

--- src/main/java/cn/osxm/ssmi/chp05/anno/ResourceAnno.java
+++ src/main/java/cn/osxm/ssmi/chp05/anno/ResourceAnno.java
@@ -8,13 +8,13 @@ import org.springframework.context.ApplicationContext;
 import javax.annotation.Resource;
 
 public class ResourceAnno {
-    @Resource(name = "foo")
+    @Resource(name = "foo")//注解使用在属性上，注入注解自定义类的对象//通过名字查找容器中对应的对象
     private Foo foo;
     private Bar bar;
-    @Resource
+    @Resource//注解使用在属性上，注入容器类的对象
     private ApplicationContext context;
 
-    @Resource
+    @Resource//注解使用在setter方法上，注入参数定义的对象
     public void setBar(Bar bar) {
         this.bar = bar;
     }
