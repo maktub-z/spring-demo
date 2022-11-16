de382568878df336c6e67e8921cddece6527f73e
zhangxiaolong <mak_tub@163.com>
2022-11-17 07:33:31 +0800

    @Component & 包扫描

---
+++ src/main/java/cn/osxm/ssmi/chp05/anno/MyTypeFilter.java
@@ -0,0 +1,26 @@
+package cn.osxm.ssmi.chp05.anno;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.ClassMetadata;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.core.type.filter.TypeFilter;
+
+import java.io.IOException;
+
+public class MyTypeFilter implements TypeFilter {
+    @Override
+    public boolean match(MetadataReader metadataReader, MetadataReaderFactory metadataReaderFactory) throws IOException {
+        AnnotationMetadata annotationMetadata = metadataReader.getAnnotationMetadata();
+        ClassMetadata classMetadata = metadataReader.getClassMetadata();
+        Resource resource = metadataReader.getResource();
+        String className = classMetadata.getClassName();
+        System.out.println("Class:-->" + className);
+
+        if (className.contains("UserController")) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/cn/osxm/ssmi/com/anno/UserController.java
@@ -0,0 +1,7 @@
+package cn.osxm.ssmi.com.anno;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+}
