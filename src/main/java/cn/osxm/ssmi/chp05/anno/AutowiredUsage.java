637aa4a1cef51f7a51e0337f718373c40b89ac88
zhangxiaolong <mak_tub@163.com>
2022-11-17 07:22:45 +0800

    @Autowired

---
+++ src/main/java/cn/osxm/ssmi/chp05/anno/AutowiredUsage.java
@@ -0,0 +1,38 @@
+package cn.osxm.ssmi.chp05.anno;
+
+import cn.osxm.ssmi.chp04.model.Bar;
+import cn.osxm.ssmi.com.Baz;
+import cn.osxm.ssmi.com.Foo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+import java.util.Set;
+
+public class AutowiredUsage {
+    private Foo foo;
+    private Bar bar;
+    //使用在属性定义上
+    @Autowired
+    private Baz baz;
+    @Autowired//自动装载数组类型的依赖
+    private Baz[] bazs;
+    @Autowired//自动装载集合类型的依赖
+    private Set<Baz> bazSet;
+    @Autowired//自动装载键值对类型的依赖
+    private Map<String, Baz> bazMap;
+
+    @Autowired                          //自动装载注解使用在构造器中
+    public AutowiredUsage(Foo foo) {
+        this.foo = foo;
+    }
+
+    @Autowired                          //在属性的 Setter 方法中使用
+    public void setBar(Bar bar) {
+        this.bar = bar;
+    }
+
+    @Autowired                          //使用在一般方法中
+    public void myInitBar(Bar bar) {
+        this.bar = bar;
+    }
+}
